450c881ce0eecbb16fb582cc314f2555052c1e0b
Omorinsola99 <morinsolashobite@gmail.com>
2026-04-11 16:53:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
 
 # ------------------ CONFIG ------------------
 st.set_page_config(page_title="Incident Performance Dashboard", layout="wide")
